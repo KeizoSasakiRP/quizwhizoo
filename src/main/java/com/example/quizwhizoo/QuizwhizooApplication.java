bee71191988ae6e7e04138c3d30edb4d90df8b5b
Keizo Sasaki <keizo.sasaki@rakus-partners.co.jp>
2024-05-23 10:11:08 +0900

    initial commit

---
+++ src/main/java/com/example/quizwhizoo/QuizwhizooApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizwhizoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizwhizooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizwhizooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quizwhizoo/QuizwhizooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizwhizoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizwhizooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
